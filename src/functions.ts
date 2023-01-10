4ece3921a7ede22c301987a1d01641159be8ba99
Sebastian Edelmeier <sebastian.edelmeier@oerlikon.com>
2023-01-11 00:40:35 +0100

    removed obsolete functions

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,3 @@
-import { Month } from "./MonthPicker";
-
 export const parseDate = (input: Date): string => {
   return new Date(input).toLocaleDateString("de-DE");
 };
@@ -10,29 +8,3 @@ export function getMonday(date: Date): Date {
   monday.setDate(date.getDate() - weekdayOfInput + 1);
   return monday;
 }
-
-export const toDate = (input: string): Date => {
-  const dateParser = /(\d{2})\.(\d{2})\.(\d{4})/;
-  const match = input.match(dateParser);
-  if (!match) {
-    throw Error("invalid Date");
-  }
-  const date = Date.UTC(+match[3], +match[2] - 1, +match[1]);
-  return new Date(date);
-};
-
-export const YYYYMMToMonth = (input: string): Month => {
-  const year = input.substring(0, 4);
-  const month = input.substring(4, 6);
-  return { monthIndex: +month - 1, year: +year };
-};
-
-export const MonthToYYYYMM = (input: Month): string => {
-  return `${input.year}${(input.monthIndex + 1).toString().padStart(2, "0")}`;
-};
-
-// export const monthForReporting = (month: number): string => {
-//   const yearPart = month.toString().substring(0, 4);
-//   const monthPart = month.toString().substring(4, 6);
-//   return `${months[+monthPart - 1]} ${yearPart}`;
-// };
